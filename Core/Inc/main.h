16671db670ef845905159be4646ee12f86299c48
kaidegit <2857693944@qq.com>
2022-09-10 23:34:12 +0800

    初始化串口

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -7,4 +7,9 @@
 
 #define LED_PIN PICO_DEFAULT_LED_PIN
 
+#define LOG_UART_NUM uart0
+
+#define LOG_UART_TX 16
+#define LOG_UART_RX -1
+
 #endif //PICO_TEMPLATE_MAIN_H
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -10,10 +10,12 @@
 int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    uart_init(LOG_UART_NUM, 115200);
+    gpio_set_function(LOG_UART_TX, GPIO_FUNC_UART);
+
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(1000);
-        gpio_put(LED_PIN, 0);
+        uart_puts(LOG_UART_NUM, " Hello, UART!\n");
         sleep_ms(1000);
     }
 }
