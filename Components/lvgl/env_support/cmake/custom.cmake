3d3916ba3c3aba48aad308d87c2d95a81aa6be69
kaidegit <2857693944@qq.com>
2022-09-16 01:00:32 +0800

    将lvgl的example和demo去除编译

--- Components/lvgl/env_support/cmake/custom.cmake
+++ Components/lvgl/env_support/cmake/custom.cmake
@@ -18,10 +18,10 @@ file(GLOB_RECURSE DEMO_SOURCES ${LVGL_ROOT_DIR}/demos/*.c)
 
 add_library(lvgl STATIC ${SOURCES})
 add_library(lvgl::lvgl ALIAS lvgl)
-add_library(lvgl_examples STATIC ${EXAMPLE_SOURCES})
-add_library(lvgl::examples ALIAS lvgl_examples)
-add_library(lvgl_demos STATIC ${DEMO_SOURCES})
-add_library(lvgl::demos ALIAS lvgl_demos)
+#add_library(lvgl_examples STATIC ${EXAMPLE_SOURCES})
+#add_library(lvgl::examples ALIAS lvgl_examples)
+#add_library(lvgl_demos STATIC ${DEMO_SOURCES})
+#add_library(lvgl::demos ALIAS lvgl_demos)
 
 target_compile_definitions(
   lvgl PUBLIC $<$<BOOL:${LV_LVGL_H_INCLUDE_SIMPLE}>:LV_LVGL_H_INCLUDE_SIMPLE>
@@ -31,13 +31,13 @@ target_compile_definitions(
 target_include_directories(lvgl SYSTEM PUBLIC ${LVGL_ROOT_DIR} ${LV_CONF_DIR})
 
 # Include /examples folder
-target_include_directories(lvgl_examples SYSTEM
-                           PUBLIC ${LVGL_ROOT_DIR}/examples)
-target_include_directories(lvgl_demos SYSTEM
-                           PUBLIC ${LVGL_ROOT_DIR}/demos)
+#target_include_directories(lvgl_examples SYSTEM
+#                           PUBLIC ${LVGL_ROOT_DIR}/examples)
+#target_include_directories(lvgl_demos SYSTEM
+#                           PUBLIC ${LVGL_ROOT_DIR}/demos)
 
-target_link_libraries(lvgl_examples PUBLIC lvgl)
-target_link_libraries(lvgl_demos PUBLIC lvgl)
+#target_link_libraries(lvgl_examples PUBLIC lvgl)
+#target_link_libraries(lvgl_demos PUBLIC lvgl)
 target_link_libraries(lvgl PUBLIC pico_stdlib)
 
 # Lbrary and headers can be installed to system using make install
